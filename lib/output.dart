d56a0e352a7bfe9cd167234867c39b6ec731e5a4
Sunny Yoo <isunyoo@gmail.com>
2021-11-13 20:54:19 +0800

    Updated for changes.

--- lib/output.dart
+++ lib/output.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import 'dart:convert';
+// import 'dart:convert';
 import 'package:pdf/pdf.dart';
 import 'utilities/web3dartutil.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +8,9 @@ import 'package:printing/printing.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:eth_toto_board_flutter/main.dart';
+import 'dart:convert' show utf8;
+import 'dart:typed_data' show Uint8List;
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class Output extends StatefulWidget {
   final List passedValue1;
@@ -56,10 +59,45 @@ class _OutputState extends State<Output> {
     );
   }
 
+  // https://stackoverflow.com/questions/62261619/how-to-upload-json-file-in-firebase-storage-flutter
+  // https://firebase.flutter.dev/docs/storage/usage/
+  // Uploading raw data
+  Future<void> uploadData() async {
+    String text = 'Hello World!';
+    List<int> encoded = utf8.encode(text);
+    Uint8List data = Uint8List.fromList(encoded);
+
+    firebase_storage.Reference ref =
+    firebase_storage.FirebaseStorage.instance.ref('uploads/hello-world.text');
+
+    try {
+      // Upload raw data.
+      await ref.putData(data);
+      // Get raw data.
+      Uint8List? downloadedData = await ref.getData();
+      // prints -> Hello World!
+      print(utf8.decode(downloadedData!));
+    } on firebase_core.FirebaseException catch (e) {
+      // e.g, e.code == 'canceled'
+    }
+  }
+
   // PDF Creation
   Future<void> receiptPDF() async {
     // Get transaction details
     String txReceipt = await web3util.getTransactionDetails(widget.passedValue3);
+    // print(txReceipt); // TransactionReceipt{transactionHash: 5ece5c28ea429e68223f89e9f0055a69027887106bfc6a83e13302d0ed05c691, transactionIndex: 4, blockHash: ba3a10618dcfbfcc1beedd8dbf9579475a7a6c37e5db4273b620eebd5614892c, blockNumber: 11419086, from: 0x8bb0412fcd5cb1a190b38db667539cc6301890e1, to: 0x4cd48e0fb3facf420a44773fb45d9382fe49f0a0, cumulativeGasUsed: 442721, gasUsed: 150884, contractAddress: null, status: true, logs: []}
+    // print(txReceipt.runtimeType);  // String
+    var jsonString = jsonEncode(txReceipt);
+    print(jsonString); // "TransactionReceipt{transactionHash: dd17c111c80c823d7334fc02a6cb741c8a7cd32f77930a7c1576900439b8525b, transactionIndex: 3, blockHash: cd01877f982bb66a890fc35ff2d5996b1ef334924a9c0ed4dcdbbcdd6c12c1b6, blockNumber: 11419131, from: 0x8bb0412fcd5cb1a190b38db667539cc6301890e1, to: 0x4cd48e0fb3facf420a44773fb45d9382fe49f0a0, cumulativeGasUsed: 299735, gasUsed: 55139, contractAddress: null, status: true, logs: []}"
+    print(jsonString.runtimeType);  // String
+    var bytes = utf8.encode(jsonString);
+    print(bytes);
+    print(bytes.runtimeType);  // Uint8List
+    // var base64Str = base64.encode(bytes);
+    // var arr = base64.decode(base64Str); //arr will be Uint8list
+    // print(arr);
+    // print(arr.runtimeType);
     if(txReceipt.length<10) {
       _showDialog(context);
     } else if(txReceipt.length>200) {
