f25e32b46e1b38543bbc0b6498bc6a7e5743995c
isunyoo <isunyoo@gmail.com>
2021-11-24 12:47:34 +0800

    Updated for changes.

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -163,3 +163,5 @@ class _LoginPageState extends State<LoginPage> {
 
 // https://blog.codemagic.io/firebase-authentication-google-sign-in-using-flutter/
 // https://pub.dev/packages/google_sign_in/example
+
+sdksds
